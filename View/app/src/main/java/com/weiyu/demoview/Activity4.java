abd175be6d6dbd2e0164c4495232fc34f0fe983a
weiyu <qning168@126.com>
2020-05-09 22:25:12 +0800

    ui demo

---
+++ View/app/src/main/java/com/weiyu/demoview/Activity4.java
@@ -0,0 +1,14 @@
+package com.weiyu.demoview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity4);
+    }
+}
